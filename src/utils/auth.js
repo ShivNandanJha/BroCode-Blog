42bb0eff5c98275d0a17840ecc5773a86e605c32
Shiv Nandan Jha <neureto12@gmail.com>
2023-09-19 20:30:09 +0530

    initial commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,6 +16,7 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  secret:process.env.NEXTAUTH_SECRET,
 };
 
 export const getAuthSession = () => getServerSession(authOptions);
