f2f823355a8eeaddb3d954198fc7210fdd828b24
Shiv Nandan Jha <neureto12@gmail.com>
2023-09-20 10:12:22 +0530

    initial commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -8,8 +8,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId    : process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
