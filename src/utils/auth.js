92e9d5a7aa79517dbd0190e8164157ed38696aad
Shiv Nandan Jha <neureto12@gmail.com>
2023-09-20 09:57:39 +0530

    initial commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,18 +7,6 @@ import { getServerSession } from "next-auth";
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-{    id: 'google',
-    name            : 'Google',
-    type            : 'oauth',
-    version         : '2.0',
-    scope           : 'openid profile email',
-    params          : { grant_type: 'authorization_code' },
-    accessTokenUrl  : 'https://accounts.google.com/o/oauth2/token',
-    requestTokenUrl : 'https://accounts.google.com/o/oauth2/auth',
-    authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
-    profileUrl      : 'https://www.googleapis.com/oauth2/v1/userinfo',
-    clientId        : process.env.GOOGLE_ID,
-    clientSecret    : process.env.GOOGLE_SECRET},
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
